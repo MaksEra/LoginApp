d43087fc14316e244c89111482316c24c28b09d9
Maks Era <maks9306@gmail.com>
2021-05-31 02:21:03 +0300

    Change extensions

--- LoginApp/ViewControllers/LoginViewController.swift
+++ LoginApp/ViewControllers/LoginViewController.swift
@@ -54,33 +54,3 @@ class LoginViewController: UIViewController {
     }
     
 }
-
-extension LoginViewController {
-    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) {
-            _ in textField?.text = ""
-        }
-        
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
-}
-
-extension LoginViewController: UITextFieldDelegate {
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == userNameTextField {
-            passwordTextField.becomeFirstResponder()
-        } else {
-            loginPressed()
-            performSegue(withIdentifier: "showWelcomeVC", sender: nil)
-        }
-        return true
-    }
-}
-
--- LoginApp/ViewControllers/WelcomeViewController.swift
+++ LoginApp/ViewControllers/WelcomeViewController.swift
@@ -34,15 +34,3 @@ class WelcomeViewController: UIViewController {
         welcomeLabel.text = "Welcome, \(user)"
     }
 }
-
-extension UIView {
-    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
-        let gradient = CAGradientLayer()
-        gradient.frame = bounds
-        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
-        gradient.locations = [0.0, 1.0]
-        gradient.startPoint = CGPoint(x: 0, y: 0)
-        gradient.endPoint = CGPoint(x: 0, y: 1)
-        layer.insertSublayer(gradient, at: 0)
-    }
-}
